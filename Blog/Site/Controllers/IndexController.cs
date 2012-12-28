0a65d2ff22c76dd3fea4176af116aab084fb73a3
Luke McGregor <luke@mcgregor.org.nz>
2012-12-28 16:16:08 +1300

    All Posts page

---
+++ Blog/Site/Controllers/IndexController.cs
@@ -0,0 +1,34 @@
+﻿using StaticVoid.Blog.Data;
+using StaticVoid.Blog.Site.Models;
+using StaticVoid.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StaticVoid.Blog.Site.Gravitar;
+
+namespace StaticVoid.Blog.Site.Controllers
+{
+    public class IndexController : Controller
+    {
+        private readonly IRepository<Post> _postRepo;
+
+        public IndexController(IRepository<Post> postRepo)
+        {
+            _postRepo = postRepo;
+        }
+        public ActionResult Posts()
+        {
+            return View(_postRepo
+                .PublishedPosts(p => p.Author)
+                .OrderByDescending(p=>p.Posted)
+                .Select(p => new PostRowIndexModel
+            {
+                Title = p.Title,
+                Url = p.Path,
+                Posted = p.Posted
+            }));
+        }
+    }
+}
---
+++ Blog/Site/Models/IndexModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StaticVoid.Blog.Site.Models
+{
+    public class PostRowIndexModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime Posted { get; set; }
+    }
+}

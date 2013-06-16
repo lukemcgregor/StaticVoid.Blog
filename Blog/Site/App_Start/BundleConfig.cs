affb450d66ca275d29f23da0ed659115e9ccb1f2
Luke McGregor <luke@mcgregor.org.nz>
2013-06-16 21:04:08 +1200

    Using a CDN + new recovery bits

--- Blog/Site/App_Start/BundleConfig.cs
+++ Blog/Site/App_Start/BundleConfig.cs
@@ -9,54 +9,32 @@ namespace StaticVoid.Blog.Site
 	{
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-1.9.1.js"));
-
 			bundles.Add(new ScriptBundle("~/bundles/openid").Include(
 						"~/Scripts/openid-jquery.js",
 						"~/Scripts/openid-en.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/blog-admin").Include(
-                        "~/Scripts/jquery-1.9.1.js",
-                        "~/Scripts/modernizr-*",
-                        "~/Scripts/Prettify/prettify.js",
                         "~/Scripts/Pure/pure.js",
-                        "~/Scripts/bootstrap.js",
                         "~/Scripts/modals.js",
-                        "~/Scripts/knockout-2.2.1.js",
-                        "~/Scripts/knockout.validation.js",
-                        "~/Scripts/validation-rules.js",
-                        "~/Scripts/knockout.mapping-latest.js"));
+                        "~/Scripts/validation-rules.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/editor").Include(
 						"~/Scripts/MarkdownDeep.js",
 						"~/Scripts/MarkdownDeepEditor.js",
 						"~/Scripts/MarkdownDeepEditorUI.js",
-						"~/Scripts/jquery.ba-resize.js",
-                        "~/Scripts/editor.js", 
-                        "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/jquery.validate.js",
-                        "~/Scripts/jquery.validate.unobtrusive.js"));
+                        "~/Scripts/editor.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/blog").Include(
-                        "~/Scripts/jquery-1.9.1.js",
-                        "~/Scripts/modernizr-*",
-                        "~/Scripts/bootstrap.js",
-                        "~/Scripts/modals.js",
-                        "~/Scripts/Prettify/prettify.js",
-                        "~/Scripts/knockout-2.2.1.js",
-                        "~/Scripts/knockout.mapping-latest.js",
-                        "~/Scripts/jquery.scrollTo.js",
-                        "~/Scripts/date.js"));
+                        "~/Scripts/modals.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css/openid").Include(
 						"~/Content/openid-shadow.css",
 						"~/Content/openid.css"));
 
 			var post = new StyleBundle("~/Content/post_less").Include(
+						"~/Content/blog.css",
 						"~/Content/post.less",
-						"~/Content/trendy-date.less",
-						"~/Content/Prettify/prettify.css");
+						"~/Content/trendy-date.less");
 			post.Transforms.Clear();
 			post.Transforms.Add(new LessTransform());
 			post.Transforms.Add(new CssMinify());
@@ -76,15 +54,7 @@ namespace StaticVoid.Blog.Site
             dashboard.Transforms.Add(new CssMinify());
             bundles.Add(dashboard);
 
-			bundles.Add(new StyleBundle("~/Content/post").Include(
-						"~/Content/bootstrap.css",
-						"~/Content/bootstrap-responsive.css",
-						"~/Content/Prettify/prettify.css",
-						"~/Content/blog.css"));
-
             bundles.Add(new StyleBundle("~/Content/blog-admin").Include(
-                        "~/Content/bootstrap.css",
-                        "~/Content/bootstrap-responsive.css",
                         "~/Content/mdd_styles.css",
                         "~/Content/blog.css",
                         "~/Content/blog-admin.css"));
---
+++ Blog/Site/Areas/Manage/Controllers/RecoveryController.cs
@@ -0,0 +1,74 @@
+﻿using StaticVoid.Blog.Data;
+using StaticVoid.Blog.Site.Controllers;
+using StaticVoid.Blog.Site.Services;
+using StaticVoid.Repository;
+using StaticVoid.Xml.DataContractSerialiser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticVoid.Blog.Site.Areas.Manage.Controllers
+{
+    public class RecoveryController : BlogBaseController
+    {
+        private readonly IRepository<Post> _postRepository;
+        private readonly IRepository<Redirect> _redirectRepository;
+        private readonly IRepository<Style> _styleRepository;
+        private readonly IRepository<Data.Blog> _blogRepository;
+        private readonly ISecurityHelper _securityHelper;
+
+        public RecoveryController(
+            IRepository<Post> postRepository,
+            IRepository<Redirect> redirectRepository,
+            IRepository<Style> styleRepository,
+            IRepository<Data.Blog> blogRepository,
+            ISecurityHelper securityHelper,
+            IHttpContextService httpContext)
+            : base(blogRepository, httpContext)
+        {
+            _postRepository = postRepository;
+            _redirectRepository = redirectRepository;
+            _styleRepository = styleRepository;
+            _blogRepository = blogRepository;
+            _securityHelper = securityHelper;
+        }
+
+        public ActionResult Recovery()
+        {
+            return PartialView("Recovery");
+        }
+
+        public FileResult Backup()
+        {
+            var backup = new BlogBackup
+            {
+                Posts = _postRepository.PostsForBlog(CurrentBlog.Id).AsEnumerable(),
+                Redirects = _redirectRepository.GetRedirects(CurrentBlog.Id).AsEnumerable()                
+            };
+
+            if (CurrentBlog.StyleId.HasValue)
+            {
+                backup.BlogStyle = _styleRepository.GetById(CurrentBlog.StyleId.Value);
+            }
+
+            var xml = backup.Serialise();
+
+            return File(System.Text.Encoding.UTF8.GetBytes(xml), System.Net.Mime.MediaTypeNames.Application.Octet, "test.xml");
+        }
+
+        [HttpPost]
+        public ActionResult Restore(HttpPostedFileBase file)
+        {
+            throw new NotImplementedException();
+        }
+
+        class BlogBackup
+        {
+            public IEnumerable<Post> Posts { get; set; }
+            public IEnumerable<Redirect> Redirects { get; set; }
+            public Style BlogStyle { get; set; }
+        }
+    }
+}
